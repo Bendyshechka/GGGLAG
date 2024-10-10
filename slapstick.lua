96f9a2a63b30f14382de413524471183d34475d4
Bendyshechka <sus003707@gmail.com>
2024-10-10 22:37:06 +0500

    Update slapstick.lua

--- slapstick.lua
+++ slapstick.lua
@@ -1,12 +1,28 @@
-OldSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
+while true do
+			-- Отключаем частицы
+			local function disableParticlesIn(parent)
+				for _, obj in pairs(parent:GetDescendants()) do
+					if obj:IsA("ParticleEmitter") or obj:IsA("Fire") or obj:IsA("Smoke") or obj:IsA("Sparkles") then
+						obj.Enabled = false
+					end
+				end
+			end
+
+			-- Поиск во всех сервисах
+			disableParticlesIn(game.Workspace)
+			disableParticlesIn(game.ReplicatedStorage)
+			disableParticlesIn(game.Lighting)
+			disableParticlesIn(game.StarterGui)
+			disableParticlesIn(game.StarterPack)
+			disableParticlesIn(game.Players)
+			disableParticlesIn(game.Teams)
+			disableParticlesIn(game.SoundService)
+
+			-- Выполняем взрыв серверного события
+			OldSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
 game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
 game:GetService("ReplicatedStorage").slapstick:FireServer("runeffect")
-wait(5)
-game:GetService("ReplicatedStorage").slapstick:FireServer("fullcharged")
-wait(1)
-OrionLib:MakeNotification({Name = "Error",Content = "Started RUN Now.",Image = "rbxassetid://7733658504",Time = 5})
-game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 70
-wait(25)
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = OldSpeed
-game:GetService("ReplicatedStorage").slapstick:FireServer("cancelrun")
+
+			-- Ожидание с минимальным временем для предотвращения зависания
+			wait(0.00000001)
+		end
