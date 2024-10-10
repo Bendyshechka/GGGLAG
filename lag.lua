528a1352ba68255a05268b5de94bbee959531d19
Bendyshechka <sus003707@gmail.com>
2024-10-10 21:56:58 +0500

    Update lag.lua

--- lag.lua
+++ lag.lua
@@ -7,47 +7,10 @@ local Tab = Window:MakeTab({
 	PremiumOnly = false
 })
 
-Tab:AddButton({
-	Name = "Включить антилаг🤫🧏‍♂️",
-	Callback = function()
-      		local function deleteSlapstickObjects(parent)
-    for _, obj in pairs(parent:GetDescendants()) do
-        if obj.Name == "slapstick" then
-            obj:Destroy() -- Удаляем объект
-        end
-    end
-end
-
--- Поиск и удаление во всех основных сервисах игры
-deleteSlapstickObjects(game.Workspace)
-deleteSlapstickObjects(game.ReplicatedStorage)
-deleteSlapstickObjects(game.ServerStorage)
-deleteSlapstickObjects(game.Lighting)
-deleteSlapstickObjects(game.StarterGui)
-deleteSlapstickObjects(game.StarterPack)
-deleteSlapstickObjects(game.Players)
-deleteSlapstickObjects(game.Teams)
-deleteSlapstickObjects(game.SoundService)
-deleteSlapstickObjects(game.Chat)
-deleteSlapstickObjects(game.HttpService)
-deleteSlapstickObjects(game.MarketplaceService)
-deleteSlapstickObjects(game.MessagingService)
-deleteSlapstickObjects(game.PolicyService)
-deleteSlapstickObjects(game.PointsService)
-deleteSlapstickObjects(game.RunService)
-deleteSlapstickObjects(game.ScriptContext)
-deleteSlapstickObjects(game.Stats)
-deleteSlapstickObjects(game.TestService)
-deleteSlapstickObjects(game.TextService)
-deleteSlapstickObjects(game.UserInputService)
-deleteSlapstickObjects(game.VRService)
-deleteSlapstickObjects(game.InsertService)
-deleteSlapstickObjects(game.LocalizationService)
-deleteSlapstickObjects(game.LogService)
-deleteSlapstickObjects(game.NetworkClient)
-deleteSlapstickObjects(game.NetworkServer)
-deleteSlapstickObjects(game.Selection)
-deleteSlapstickObjects(game.SocialService)
-
-  	end    
+Tab:AddToggle({
+	Name = "Включить взрыв сервера😈",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
 })
