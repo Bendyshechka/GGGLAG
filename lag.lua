a013a185b6ca7d33632e424072a1879ab47ef762
Bendyshechka <sus003707@gmail.com>
2024-10-10 22:04:26 +0500

    Update lag.lua

--- lag.lua
+++ lag.lua
@@ -13,8 +13,56 @@ Tab:AddToggle({
 	Callback = function(Value)
 		SlapstickSpam = Value
 		while SlapstickSpam do
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Bendyshechka/GGGLAG/refs/heads/main/antilag.lua"))()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Bendyshechka/GGGLAG/refs/heads/main/slapstick.lua"))()
+		local function disableParticlesIn(parent)
+    for _, obj in pairs(parent:GetDescendants()) do
+        if obj:IsA("ParticleEmitter") or obj:IsA("Fire") or obj:IsA("Smoke") or obj:IsA("Sparkles") then
+            obj.Enabled = false
+        end
+    end
+end
+
+-- Поиск во всех сервисах
+disableParticlesIn(game.Workspace)
+disableParticlesIn(game.ReplicatedStorage)
+disableParticlesIn(game.ServerStorage)
+disableParticlesIn(game.Lighting)
+disableParticlesIn(game.StarterGui)
+disableParticlesIn(game.StarterPack)
+disableParticlesIn(game.Players)
+disableParticlesIn(game.Teams)
+disableParticlesIn(game.SoundService)
+disableParticlesIn(game.Chat)
+disableParticlesIn(game.HttpService)
+disableParticlesIn(game.MarketplaceService)
+disableParticlesIn(game.MessagingService)
+disableParticlesIn(game.PolicyService)
+disableParticlesIn(game.PointsService)
+disableParticlesIn(game.RunService)
+disableParticlesIn(game.ScriptContext)
+disableParticlesIn(game.Stats)
+disableParticlesIn(game.TestService)
+disableParticlesIn(game.TextService)
+disableParticlesIn(game.UserInputService)
+disableParticlesIn(game.VRService)
+disableParticlesIn(game.InsertService)
+disableParticlesIn(game.LocalizationService)
+disableParticlesIn(game.LogService)
+disableParticlesIn(game.NetworkClient)
+disableParticlesIn(game.NetworkServer)
+disableParticlesIn(game.Selection)
+disableParticlesIn(game.SocialService)
+		OldSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+game:GetService("ReplicatedStorage").slapstick:FireServer("runeffect")
+wait(5)
+game:GetService("ReplicatedStorage").slapstick:FireServer("fullcharged")
+wait(1)
+OrionLib:MakeNotification({Name = "Error",Content = "Started RUN Now.",Image = "rbxassetid://7733658504",Time = 5})
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 70
+wait(25)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = OldSpeed
+game:GetService("ReplicatedStorage").slapstick:FireServer("cancelrun")
 		wait(1)
 		task.wait()
 end
