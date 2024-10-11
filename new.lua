eae12be8df70bf3b7ec1949cd7836d2d75b61d86
Bendyshechka <sus003707@gmail.com>
2024-10-11 18:24:44 +0500

    Update new.lua

--- new.lua
+++ new.lua
@@ -40,8 +40,6 @@ Tab2:AddToggle({
 	Default = false,
 	Callback = function(Value)
 SlapstickSpam = Value
-if SlapstickSpam == true then
-game:GetService("ReplicatedStorage").slapstick:FireServer("addarm")
 while SlapstickSpam do
 OldSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
 game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
@@ -49,9 +47,6 @@ game:GetService("ReplicatedStorage").slapstick:FireServer("runeffect")
 wait(5)
 game:GetService("ReplicatedStorage").slapstick:FireServer("fullcharged")
 wait(1)
-OrionLib:MakeNotification({Name = "Error",Content = "Started RUN Now.",Image = "rbxassetid://7733658504",Time = 5})
-game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 70
 wait(0.001)
 	end    
 })
