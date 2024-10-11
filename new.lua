f63ad77bedc1ec0d9f547ab95cedc3c0a56055d6
Bendyshechka <sus003707@gmail.com>
2024-10-11 18:22:59 +0500

    Update new.lua

--- new.lua
+++ new.lua
@@ -43,7 +43,15 @@ SlapstickSpam = Value
 if SlapstickSpam == true then
 game:GetService("ReplicatedStorage").slapstick:FireServer("addarm")
 while SlapstickSpam do
-game:GetService("ReplicatedStorage").slapstick:FireServer(SlapstickAbility)
-task.wait()
+OldSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+game:GetService("ReplicatedStorage").slapstick:FireServer("runeffect")
+wait(5)
+game:GetService("ReplicatedStorage").slapstick:FireServer("fullcharged")
+wait(1)
+OrionLib:MakeNotification({Name = "Error",Content = "Started RUN Now.",Image = "rbxassetid://7733658504",Time = 5})
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 70
+wait(0.001)
 	end    
 })
