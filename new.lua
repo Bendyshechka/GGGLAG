aeab8ae277d9dc09e1431e2683d90e6a3dfbab62
Bendyshechka <sus003707@gmail.com>
2024-10-11 18:39:23 +0500

    Update new.lua

--- new.lua
+++ new.lua
@@ -26,20 +26,9 @@ Tab1:AddButton({
   	end    
 })
 
-Tab1:AddButton({
+Tab2:AddButton({
 	Name = "Spam Ability Slapstick",
 	Callback = function()
-OldSpeed = game.Players.LocalPlayer.Character.Humanoid.WalkSpeed
-game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
-game:GetService("ReplicatedStorage").slapstick:FireServer("runeffect")
-wait(5)
-game:GetService("ReplicatedStorage").slapstick:FireServer("fullcharged")
-wait(1)
-OrionLib:MakeNotification({Name = "Error",Content = "Started RUN Now.",Image = "rbxassetid://7733658504",Time = 5})
-game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 70
-wait(25)
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = OldSpeed
-game:GetService("ReplicatedStorage").slapstick:FireServer("cancelrun")
+		print("sss")
   	end    
 })
